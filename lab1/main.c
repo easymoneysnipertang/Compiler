718e45c121f9e6f2b1bb0d16b222ad738ed67786
tang <876836336@qq.com>
2023-09-08 17:34:47 +0800

    LLVM IR

--- lab1/main.c
+++ lab1/main.c
@@ -1,16 +1,24 @@
 #include<stdio.h>
-#define n 3
-
-void do_while(int i,int f){
+int main(){
+    int i,n,f;
+    scanf("%d",&n);// 接收输入
+    i=2;// 初始化循环计数器
+    f=1;
     while(i<=n){
         f=f*i;// 累乘计算
         i=i+1;
     }
+    printf("%d\n",f);
 }
 
-int main(){
-    int i,f;
-    i=2;// 初始化循环计数器
-    f=1;
-    do_while(i,f);
-}
+
+// int main(){
+//     int i,n,f;
+//     n=2;
+//     i=2;// 初始化循环计数器
+//     f=1;
+//     while(i<=n){
+//         f=f*i;// 累乘计算
+//         i=i+1;
+//     }
+// }
